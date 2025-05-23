fda57246966b25947b9a7e524e1c421da53020e3
smhrash <smhrash@gmail.com>
2025-05-23 11:42:04 -0400

    Test runner updated

--- src/test/java/com/perficient/sarker/runners/TestRunner.java
+++ src/test/java/com/perficient/sarker/runners/TestRunner.java
@@ -2,6 +2,7 @@ package com.perficient.sarker.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
@@ -15,6 +16,11 @@ import io.cucumber.testng.CucumberOptions;
 //        tags = "@patch-booking"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
 
 
